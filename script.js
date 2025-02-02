dfb472f3c05c3e0804527f79d4fb74f4c61ca800
floppa9 <payyypallll50@gmail.com>
2025-02-02 20:30:43 +0100

    enhace

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ input.addEventListener('input', () => {
     }
   });
 });
+
+const navButtons = document.querySelectorAll('.nav-buttons button');
+const navbar = document.querySelector('nav');
+
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    navbar.style.display = 'none'; // Hide navbar
+  });
+});
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > lastScrollY) { // scrolling down
+        nav.style.transform = 'translateY(-100%)';
+    } else { // scrolling up
+        nav.style.transform = 'translateY(0)';
+    }
+    lastScrollY = window.scrollY;
+});
